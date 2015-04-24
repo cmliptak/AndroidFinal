3de9877390056f91662ba73e35f4f46b0336cfa7
cmliptak <cmliptak@gmail.com>
2015-04-24 14:03:24 -0600

    Tuition Function
    finished the tuition calculations, still needs validation

--- app/src/main/java/com/adam/christina/androidfinal/TuitionCalc.java
+++ app/src/main/java/com/adam/christina/androidfinal/TuitionCalc.java
@@ -4,14 +4,32 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 
 public class TuitionCalc extends ActionBarActivity {
+
+    private int numberOfCredits;
+    private double baseCost = 300.00;
+    private double discount;
+    private double grants = 00.00;
+    private double finAid = 00.00;
+    private double bookCost = 00.00;
+    private double total = 00.00;
+    //gui variables
+    TextView a, b, c, d, e, f, g;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tuition_calc);
+
+        setFields();
     }
 
 
@@ -35,5 +53,52 @@ public class TuitionCalc extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setBase(){
+        numberOfCredits = Integer.parseInt(a.getText().toString());
+
+        //set base tuition
+        baseCost *= numberOfCredits;
+        b.setText("$" + baseCost);
+
+    }
+
+    public void calculateTuition(View view)
+    {
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
+        numberOfCredits = Integer.parseInt(a.getText().toString());
+
+        //set base tuition
+        baseCost *= numberOfCredits;
+        b.setText("$" + baseCost);
+
+        //set discount
+        if (checkBox.isChecked()) {
+              discount = 800.00;
+              c.setText("Instate Discount is $" + discount);
+           }
+        else{
+            discount = 00.00;
+        }
+
+        grants = Integer.parseInt(d.getText().toString());
+        finAid = Integer.parseInt(e.getText().toString());
+        bookCost = Integer.parseInt(f.getText().toString());
+        //calculate total
+        total = (baseCost - discount - grants - finAid + bookCost);
+        g.setText("$" + total);
+    }
+
+    public void setFields()
+    {
+        a = (TextView)findViewById(R.id.editText15); //# of classes
+        b = (TextView)findViewById(R.id.editText16);// tuition base
+        c = (TextView)findViewById(R.id.textView7);//instate discount
+        d = (TextView)findViewById(R.id.editText17);//grants
+        e = (TextView)findViewById(R.id.editText18);//financial aid
+        f = (TextView)findViewById(R.id.editText19);//book estimation
+        g = (TextView)findViewById(R.id.textView8); //total est. cost
+
     }
 }
